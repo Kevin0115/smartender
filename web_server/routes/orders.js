557bcfb606191d4d592bb7456a99616b97022199
Kevin0115 <kevin.choi0115@gmail.com>
2019-03-19 11:32:44 -0700

    integration tests beginning

--- web_server/routes/orders.js
+++ web_server/routes/orders.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var utils = require('../utils/utils');
+var fetch = require('node-fetch');
+
+// TEMPORARY TESTING
+var PI_URL = 'https://smartender-5t3k-qc8z.try.yaler.io/';
 
 router.get('/', function(req, res) {
   res.send('Hello World');
@@ -29,6 +33,41 @@ router.post('/', function(req, res) {
           }
         ]
       })
+    }
+  })
+})
+
+// Make an order to machine #id
+router.post('/test', function(req, res) {
+  var machine_id = req.body.machine_id;
+  var username = req.body.username;
+  var drink_id = req.body.drink_id;
+  var name = req.body.name;
+  var recipe = [];
+
+  Drinks.findOne({drink_id: drink_id})
+  .exec(function(err, drink) {
+    if (drink == undefined) {
+      res.send('Drink does not exist');
+    } else {
+      recipe = drink.recipe;
+      var order = {
+        username: username,
+        order: [
+          {
+            name: name,
+            recipe: recipe
+          }
+        ]
+      }
+
+      // TESTING ONLY - CHANGE TO ORDER
+      fetch(PI_URL + 'test/', {
+        method: 'POST',
+        body: JSON.stringify({message: 'fuck 391'}),
+      })
+      .then(res => res.json())
+      .then(json => console.log(json));
     }
   })
 })
