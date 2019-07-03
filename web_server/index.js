4edfec258cd3da443dfa73c91d3af23d64a7598f
root <root@ip-10-0-0-208.us-east-2.compute.internal>
2019-07-03 04:56:00 +0000

    https removed, no ip tables

--- web_server/index.js
+++ web_server/index.js
@@ -2,11 +2,8 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var https = require('https');
 var http = require('http');
 var fs = require('fs');
-var privateKey  = fs.readFileSync('./ssl/server.key', 'utf8');
-var certificate = fs.readFileSync('./ssl/server.cert', 'utf8');
 
 var orders = require('./routes/orders');
 var drinks = require('./routes/drinks');
@@ -14,9 +11,7 @@ var machines = require('./routes/machines');
 var users = require('./routes/users');
 
 // Declare application parameters
-var HTTP_PORT = 8000;
-var HTTPS_PORT = 8443;
-var credentials = {key: privateKey, cert: certificate};
+var HTTP_PORT = 80;
 
 // Configure
 require('./config/configuration.js')(app, mongoose);
@@ -35,11 +30,7 @@ app.use('/users', users);
 
 // Server
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(HTTP_PORT, function() {
   console.log('[Express.js] Server listening on PORT: '+ HTTP_PORT);
-});
-httpsServer.listen(HTTPS_PORT, function() {
-  console.log('[Express.js] Server listening on PORT: '+ HTTPS_PORT);
 });
