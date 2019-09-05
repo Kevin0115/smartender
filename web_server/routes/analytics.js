f5868cdc55d9a240d1b273e6298363b662198568
Kevin <kevin.choi0115@gmail.com>
2019-09-05 05:29:31 +0000

    collision handling, sort of

--- web_server/routes/analytics.js
+++ web_server/routes/analytics.js
@@ -30,6 +30,8 @@ router.get('/count', function(req, res) {
 
 // Post new site visit
 router.post('/', function(req, res) {
+  // Log every visit to console; debugging
+  console.log('New Visit ID: ' + req.body.sessionId);
   Analytics.findOne({sessionId: req.body.sessionId})
   .exec(function(err, sessionId) {
     if (err) {
@@ -45,7 +47,8 @@ router.post('/', function(req, res) {
           res.send({status: 'New session inserted'});
         }
       });
-    } else {
+    } else { 
+      if (req.body.events[0].eventType !== 'sessionStart') {
       Analytics.updateOne(
         {sessionId: req.body.sessionId},
         {$push: {events: req.body.events[0]}}
@@ -57,8 +60,28 @@ router.post('/', function(req, res) {
           res.send({status: 'Updated session events'});
         }
       });
+      } else {
+        Analytics.create({
+          sessionId: req.body.sessionId + '_' + Math.floor(Math.random() * 10),
+          events: req.body.events,
+        }, function(err, analytics) {
+          if(err) { res.send({status: 'Error: ' + err}); }
+          else { res.send({status: 'Collision session inserted'}); }
+        });
+      }
     }
   });
 })
+
+// Change user.id to proper authentication field (id) or use Passport.js
+router.post('/login', function(req, res) {
+  Users.findOne({username: req.body.username})
+  .exec(function(err, user) {
+    if (err) { res.send({status: 'Error: ' + err}); }
+    else if (user == undefined) { res.send({status: 'User not found'}); }
+    else if (req.body.password != user.id) { res.send({status: 'Incorrect Password'}); }
+    else { res.send({status: 'Authenticated'}); }
+  })
+});
 
 module.exports = router;
